c4c97447dbf3325e8ded95edc6fdd89a71099373
RuthLopesDiniz <ruthma1964@hotmail.com>
2023-06-28 17:12:35 -0300

    Sorteador funcionando

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,13 @@
 function generateNumber(){
-    const min = document.querySelector(".inputMin").value
-    const max = document.querySelector(".inputMax").value
-    console.log(min, max)
+    const min = Math.ceil( document.querySelector(".inputMin").value)
+    const max = Math.floor( document.querySelector(".inputMax").value)
+    const mensage = document.querySelector(".message")
+    const inputResult = document.querySelector(".input-result")
+
+    const resut = Math.floor(Math.random()*(max-min+1))+min;
+    mensage.innerHTML = `O número sorteado é:`
+    inputResult.innerHTML = resut
 }
+
+ 
 
