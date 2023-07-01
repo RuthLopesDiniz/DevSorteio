d7c5c9ae58c517ba2ec14df5a653b2133cbb8de5
RuthLopesDiniz <ruthma1964@hotmail.com>
2023-07-01 15:48:06 -0300

    Comparação entre números adicionada.

--- JavaScript.js
+++ JavaScript.js
@@ -4,9 +4,13 @@ function generateNumber(){
     const mensage = document.querySelector(".message")
     const inputResult = document.querySelector(".input-result")
 
+        if(min >= max){
+            alert("Primeiro valor precisa ser menor que o segundo valor!")
+        }else{
     const resut = Math.floor(Math.random()*(max-min+1))+min;
     mensage.innerHTML = `O número sorteado é:`
     inputResult.innerHTML = resut
+            }
 }
 
  
